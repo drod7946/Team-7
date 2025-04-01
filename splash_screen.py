9feb33f5584c3796f6f1e43b16021cc0f5fe90e8
A.Lee <114944839+BUZZERstrafe@users.noreply.github.com>
2025-04-01 14:26:43 -0500

    Update splash_screen.py
    Adjusted how the window size was created and centered. Hopefully it works the way it's supposed to.

--- splash_screen.py
+++ splash_screen.py
@@ -5,18 +5,34 @@ from entry_screen_final import show_entry_screen
 
 def show_splash(root):
     splash = tk.Toplevel(root)
-    splash.geometry("1600x900")
     splash.title("Splash Screen")
     splash.resizable(True, True)
 
+    screen_x = splash.winfo_screenwidth()
+    screen_height = splash.winfo_screenheight()
+    available_height = splash.winfo_vrootheight()
+
+    if available_height < screen_height:
+        screen_y = available_height
+    else:
+        screen_y =screen_height
+
+    window_x = int(screen_x * 0.9)
+    window_y = int(screen_y * 0.9)
+
+    center_x = (screen_x - window_x) // 2
+    center_y = (screen_y - window_y) // 2
+
+    splash.geometry(f"{window_x}x{window_y}+{center_x}+{center_y}")
+
     # Load the image
     image = Image.open("logo.jpg")
-    image = image.resize((1600, 900))
+    image = image.resize((window_x, window_y), Image.LANCZOS)
     photo = ImageTk.PhotoImage(image)
 
     label = tk.Label(splash, image=photo)
     label.image = photo 
-    label.pack()
+    label.pack(fill="both", expand=True)
     
     # Transition to the entry screen after 3 seconds
     splash.after(3000, start_entry_screen, root, splash)
