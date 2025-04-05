0193eaf4cdeff1af783f4d056847ffbeb8641c2c
A.Lee <114944839+BUZZERstrafe@users.noreply.github.com>
2025-04-05 17:21:30 -0500

    Adjusted dimensions for the whole screen
    May revert. Deleted scale_factor. Still hard-coded dimensions but everything should fit on a 1350x900 geometry now.

--- entry_screen_final.py
+++ entry_screen_final.py
@@ -8,11 +8,7 @@ from play_action_display import show_countdown
 def connect_to_db():
     try:
         connection = psycopg2.connect(
-            dbname="photon",
-            user="student", 
-            password="student", 
-            host="localhost", 
-            port="5432"
+            dbname="photon"
         )
         return connection
     except Exception as e:
@@ -99,67 +95,70 @@ def show_entry_screen(root):
 
     # Set the window's position
     entry_screen_window.geometry(f"1350x900+{center_x}+{center_y}")
+    entry_screen_window.update()
 
     label = customtkinter.CTkLabel(entry_screen_window, text="Edit Current Game", font=("Helvetica", 18, "bold"), text_color="#99AAFF")
     label.pack(pady=0)
 
     window_bg = entry_screen_window.cget("bg")
-    scale_factor = 0.8
+    #scale_factor = 0.8
 
     frame = Frame(entry_screen_window)
     frame.pack(fill="both", expand=True, anchor="n")
 
-    canvas = Canvas(frame, width=int(1350 * scale_factor), height=int(900 * scale_factor), bg=window_bg, highlightthickness=0)
+    canvas = Canvas(frame, width=int(1350), height=int(900), bg=window_bg, highlightthickness=0)
     canvas.pack(fill="both", expand=True, anchor="n")
 
     # red box (x1, y1, x2, y2) ; #DCDDDE = darkish white
-    canvas.create_rectangle(440, 10, 1040, 850, fill="#B30000", outline="#B30000")
-    canvas.create_rectangle(640, 20, 840, 60, outline="#DCDDDE")
-    canvas.create_text(740, 40, text="RED TEAM", font=("Helvetica", 14), fill="#DCDDDE")
+    canvas.create_rectangle(40, 10, 640, 600, fill="#B30000", outline="#B30000")
+    canvas.create_rectangle(240, 20, 440, 60, outline="#DCDDDE")
+    canvas.create_text(340, 40, text="RED TEAM", font=("Helvetica", 14), fill="#DCDDDE")
 
     # green box
-    canvas.create_rectangle(1050, 10, 1650, 850, fill="#008000", outline="#008000") 
-    canvas.create_rectangle(1250, 20, 1450, 60, outline="#DCDDDE")
-    canvas.create_text(1350, 40, text="GREEN TEAM", font=("Helvetica", 14), fill="#DCDDDE")
+    canvas.create_rectangle(710, 10, 1310, 600, fill="#008000", outline="#008000") 
+    canvas.create_rectangle(910, 20, 1110, 60, outline="#DCDDDE")
+    canvas.create_text(1010, 40, text="GREEN TEAM", font=("Helvetica", 14), fill="#DCDDDE")
 
     # grey rectangle
-    canvas.create_rectangle(825, 850, 1275, 885, fill="#808A87", outline="#808A87")
-    canvas.create_text(1050, 867, text="Game Mode: Standard public mode", font=("Helvetica", 14, "bold"), fill="#DCDDDE")
+    canvas.create_rectangle(445, 600, 905, 635, fill="#808A87", outline="#808A87")
+    canvas.create_text(675, 617, text="Game Mode: Standard public mode", font=("Helvetica", 14, "bold"), fill="#DCDDDE")
 
     entry_fields = []
 
     def create_input_fields(start_x, start_y):
-        for i in range(19):
-            y_offset = start_y + (i * 40)
+        for i in range(16):
+            y_offset = start_y + (i * 30)
             canvas.create_rectangle(start_x, y_offset, start_x + 17, y_offset + 17, outline="#DCDDDE") # check box
             canvas.create_text(start_x + 35, y_offset + 8, text=str(i), font=("Helvetica", 14), fill="#DCDDDE") # number
             
-            entry1 = Entry(entry_screen_window, font=("Helvetica", 14), bg="#DCDDDE", width=25)
-            entry2 = Entry(entry_screen_window, font=("Helvetica", 14), bg="#DCDDDE", width=25)
+            entry1 = Entry(entry_screen_window, font=("Helvetica", 14), bg="#DCDDDE", width=40)
+            entry2 = Entry(entry_screen_window, font=("Helvetica", 14), bg="#DCDDDE", width=40)
             
             canvas.create_window(start_x + 50, y_offset + 8, window=entry1, anchor=W)
-            canvas.create_window(start_x + 230, y_offset + 8, window=entry2, anchor=W)
+            canvas.create_window(start_x + 250, y_offset + 8, window=entry2, anchor=W)
             
             entry_fields.append((entry1, entry2))
 
-    create_input_fields(450, 88) # Red Team Fields
-    create_input_fields(1060, 88) # Green Team Fields
+    create_input_fields(50, 88) # Red Team Fields
+    create_input_fields(720, 88) # Green Team Fields
+
+    entry_screen_window.update_idletasks()
 
     buttons = [
         (0, "F1\nEdit\nGame"),
         (120, "F2\nGame\nParameters"),
-        (240, "F3\nStart\nGame"),
-        (600, "F5\nPreEntered\nGames"),
-        (1050, "F7"),
-        (1170, "F8\nView\nGame"),
-        (1400, "F10\nFlick\nSync"),
-        (1575, "F12\nClear\nGame")
+        (318, "F3\nStart\nGame"),
+        (516, "F5\nPreEntered\nGames"),
+        (714, "F7"),
+        (912, "F8\nView\nGame"),
+        (1110, "F10\nFlick\nSync"),
+        (1230, "F12\nClear\nGame")
     ]
 
     # Function for buttons added here
     for x, text in buttons:
         btn = Button(frame, text=text, font=("Helvetica", 11), fg="#00B300", bg="#1C2920", relief="raised", command=lambda t=text: on_button_click(t))
-        btn.place(x=    x, y=818, width=120, height=120)
+        btn.place(x=x, y=740, width=120, height=120)
 
     entry_screen_window.bind_all("<F1>", on_key_pressed)
     entry_screen_window.bind_all("<F2>", on_key_pressed)
@@ -170,9 +169,9 @@ def show_entry_screen(root):
     entry_screen_window.bind_all("<F10>", on_key_pressed)
     entry_screen_window.bind_all("<F12>", on_key_pressed)
 
-    canvas.create_rectangle(0, 1170, 2110, 1210, fill="#808A87")  # Footer background
-    canvas.create_text(1025, 1190, text="<Del> to Delete Player, <ins> to Manually Insert, or edit codename", font=("Helvetica", 12), fill="#DCDDDE")
+    canvas.create_rectangle(0, 860, 1350, 900, fill="#808A87")  # Footer background
+    canvas.create_text(675, 880, text="<Del> to Delete Player, <ins> to Manually Insert, or edit codename", font=("Helvetica", 12), fill="#DCDDDE")
 
-    canvas.scale("all", 0, 0, scale_factor, scale_factor)
+    canvas.scale("all", 0, 0, 1.0, 1.0)
 
     entry_screen_window.protocol("WM_DELETE_WINDOW", root.destroy)
